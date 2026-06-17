d49d0807370a626953800ed306f83734af5b73d5
j-hippie-dev <j.hippie.dev@gmail.com>
2026-06-17 18:37:33 +0900

    260617 컴포넌트 분리 작업 중

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
 import HeaderComp from "./components/HeaderComp.vue";
 import FooterComp from "./components/FooterComp.vue";
+import Registration from "./pages/Registration.vue";
 </script>
 
 <template>
-  <HeaderComp/>
-  <FooterComp/>
+  <Registration />
 </template>
---
+++ src/components/InputBasicComp.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+  'type': String
+  , 'placeholder': String
+  , 'readonly': Boolean
+  , 'required': Boolean
+});
+
+const model = defineModel();
+</script>
+
+<template>
+  <input
+    :type="props.type"
+    :placeholder="props.placeholder"
+    :readonly="props.readonly"
+    :required="props.required"
+    v-model="model"
+  >
+</template>
+
+<style scoped>
+
+</style>
